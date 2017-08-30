f9bd13e77dadca81a30a6a2b75152d53f89c3b5a
Ravsimar Singh Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2017-08-31 01:51:49 +0530

    fixed echo

--- echo.c
+++ echo.c
@@ -7,21 +7,39 @@ char* echo(char* arg)
     int size = 0;
     while(arg[size] != '\0') size++;
     size--;
-    if((arg[0] == '"' && arg[size] != '"') || (arg[size] == '"' && arg[0] != '"'))
+    if((size == 0 && (arg[0] == '\'' || arg[0] == '"'))||((arg[0] == '"' && arg[size] != '"') || (arg[size] == '"' && arg[0] != '"')))
+    {
         printf("Error");
+        return "";
+    }
     if((arg[0] == '\'' && arg[size] != '\'') || (arg[size] == '\'' && arg[0] != '\''))
+    {
         printf("Error");
-    if(arg[0] != '"' && arg[0] != '\'') return arg;
+        return "";
+    }
+    
     char* str = malloc(sizeof(char) * 1000);
     int k = 0;
-    
     char rem = ' ';
-    for(i=0;i<size;i++)
+    if(arg[0] != '"' && arg[0] != '\'') 
     {
-        if(rem == ' ' && (arg[i] == '"' || arg[i] == '\''))
-            rem = arg[i];
-        if(arg[i] != arg[0] && arg[i] != rem)
-            str[k++] = arg[i];
+        for(i=0;i<=size;i++)
+        {
+            if(arg[i] != '"' && arg[i] != '\'')
+                str[k++] = arg[i];
+        }
+        str[k] = '\0';
+        return str;
+    }
+    else
+    {
+        for(i=0;i<size;i++)
+        {
+            if(rem == ' ' && (arg[i] == '"' || arg[i] == '\''))
+                rem = arg[i];
+            if(arg[i] != arg[0] && arg[i] != rem)
+                str[k++] = arg[i];
+        }
     }
     str[k] = '\0';
     return str;
