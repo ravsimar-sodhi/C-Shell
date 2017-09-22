df49873d9d536d2542d0769e6c724271ad1d83d4
Ravsimar Singh Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2017-09-22 17:57:11 +0530

    redir2

--- redirection.c
+++ redirection.c
@@ -2,5 +2,11 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
-
-int checkRedirection()
+int main()
+{
+    int fd1 = creat("output.txt", 0644) ;
+    dup2(fd1, STDOUT_FILENO);
+    printf("works!");
+    close(fd1);
+    return 0;
+}
